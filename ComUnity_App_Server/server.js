3a11fb0f678a21c7e6fd7ae0058c59a0b7980d3a
pavitrakumargupta <pavitragupta021@gmail.com>
2023-07-08 03:41:02 +0530

    branch created

--- ComUnity_App_Server/server.js
+++ ComUnity_App_Server/server.js
@@ -1,13 +1,13 @@
 const express=require("express")
 const cors =require("cors")
 const userRoutes =require("./routes/routes.js")
-
+ 
 const app=express()
 require("dotenv").config();
 
 
-app.use(cors())
-app.use(express.json())
+app.use(cors())  
+app.use(express.json()) 
  
 app.use("/",userRoutes)
 
